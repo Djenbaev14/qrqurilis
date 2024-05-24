535e985bf83124038430dc917005029d53cba2c9
Djenbaev14 <89042687+Djenbaev14@users.noreply.github.com>
2024-05-24 07:35:32 +0500

    eqeq

--- public/frontend/Js/spec.js
+++ public/frontend/Js/spec.js
@@ -1,8 +1,8 @@
 
-function specBtn(e) {
-  console.log(12112);
-  e.classList.toggle("active");
-}
+document.getElementById("spec-btn").addEventListener("click", (e) => {
+  e.target.parentNode.classList.toggle("active");
+});
+
 jQuery(document).ready(function ($) {
   $(".SpecialWindow").click(function () {
     $(".OpenSpecialWindow").slideToggle(200);
